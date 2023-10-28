8ab9a539f67f11b7ff08f322de3d632064aa10e6
cht527 <cht527@126.com>
2023-10-28 20:26:53 +0800

    feat: add auth api

---
+++ api/auth.ts
@@ -0,0 +1,20 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { MongoClient } from "mongodb";
+import { CONNECTION_URL } from "./constants";
+
+module.exports = async (req: VercelRequest, res: VercelResponse) => {
+  const client = new MongoClient(CONNECTION_URL);
+
+  await client.connect();
+  console.log("Connected successfully to server");
+
+  const db = await client.db("user");
+  var result = await db
+    .collection("meta")
+    .find()
+    .toArray();
+  const data = {
+    result,
+  };
+  res.status(200).json(data);
+};
