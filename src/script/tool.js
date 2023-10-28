1ef87b8d6271fbf7e4b0bfaed5bdf79afd2caea6
cht527 <cht527@126.com>
2023-10-28 20:27:09 +0800

    feat: add indexedDB

--- src/script/tool.js
+++ src/script/tool.js
@@ -20,3 +20,98 @@ window.ANAlYZER_UTILS = {
 window.echartsUtil = {
   format: echarts.format,
 };
+
+class IndexedDBUtil {
+   db_config = {
+    dbName: 'userDB',
+    // 用于版本控制，在升级 DB 结构时，将此数字 + 1
+    dbVersion: 3,
+    storeName: 'userStore',
+    keyPath: 'id',
+  };
+  db_expiration = 24*60*60*1000;
+  dbUpdateTime = 0;
+  deleteIndexedDB = (dbName) =>
+    new Promise((resolve, reject) => {
+      const deleteDBRequest = window.indexedDB.deleteDatabase(dbName);
+
+      deleteDBRequest.onsuccess = () => {
+        console.info("IndexedDB deleted successfully.");
+        resolve();
+      };
+
+      deleteDBRequest.onerror = (event) => {
+        console.error("IndexedDB error on delete.", event);
+        reject(deleteDBRequest.error);
+      };
+    });
+
+  connectIndexedDB = (dbInformation) =>
+    new Promise((resolve, reject) => {
+      const { dbName, dbVersion, storeName, keyPath } = dbInformation;
+      const dbRequest = window.indexedDB.open(dbName, dbVersion);
+
+      dbRequest.onerror = (event) => {
+        console.error("IndexedDB error on open.", event);
+        // 打开错误时，删除用户本地 DB
+        this.deleteIndexedDB(dbName).catch(() => {});
+        reject(dbRequest.error);
+      };
+      dbRequest.onupgradeneeded = () => {
+        const db = dbRequest.result;
+
+        if (!db.objectStoreNames.contains(storeName)) {
+          db.createObjectStore(storeName, { keyPath });
+        }
+      };
+      dbRequest.onsuccess = () => {
+        resolve(dbRequest.result);
+      };
+    });
+
+  getIndexedDBData = (dbQuery) =>
+    new Promise((resolve, reject) => {
+      const { storeName, query } = dbQuery;
+
+      this.connectIndexedDB(dbQuery)
+        .then((db) => {
+          const transaction = db
+            .transaction([storeName], "readonly")
+            .objectStore(storeName)
+            .get(query);
+
+          transaction.onsuccess = () => {
+            resolve(transaction.result);
+          };
+          transaction.onerror = () => {
+            reject(transaction.error);
+          };
+        })
+        .catch(reject);
+    });
+
+  setIndexedDBData = (dbInformation, data) =>
+    new Promise((resolve, reject) => {
+      const { storeName } = dbInformation;
+
+      this.connectIndexedDB(dbInformation)
+        .then((db) => {
+          const transaction = db
+            .transaction([storeName], "readwrite")
+            .objectStore(storeName)
+            .put(data);
+
+          transaction.onsuccess = () => {
+            resolve();
+          };
+          transaction.onerror = () => {
+            reject(transaction.error);
+          };
+        })
+        .catch(reject);
+    });
+}
+
+window.apiConfig = {
+  auth: "api/auth",
+};
