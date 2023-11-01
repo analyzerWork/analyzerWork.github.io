2dd80f64a4fc4e8eeaa96bb3d96a1d0642995295
cht527 <cht527@126.com>
2023-11-01 20:20:12 +0800

    feat: remove db query

--- api/index.ts
+++ api/index.ts
@@ -7,10 +7,8 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
   await client.connect();
   console.log('Connected successfully to server');
 
-  const db = await client.db("public");
-  var result = await db.collection("bigData").find({"加工后成分":'西梅'}).toArray();
   const data = {
-    result,
+    result:[],
   };
   res.status(200).json(data);
 };
