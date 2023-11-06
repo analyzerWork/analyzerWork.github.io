c8cefe3456d098fb4be1cc8dffcb2cac0c3e302b
htCaoHaitao <95202481+htCaoHaitao@users.noreply.github.com>
2023-11-06 16:56:04 +0800

    feat: add PAGE_SIZE

--- api/_constants.ts
+++ api/_constants.ts
@@ -1,4 +1,5 @@
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_KEY}.feljs0l.mongodb.net/?retryWrites=true&w=majority`;
 const SELECT_ALL = "全部";
+const PAGE_SIZE = 50;
 
-export { CONNECTION_URL, SELECT_ALL };
+export { CONNECTION_URL, SELECT_ALL, PAGE_SIZE };
