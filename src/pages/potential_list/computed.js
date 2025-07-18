206b7bc8a6adda446fdf7bf6f35764a600aee527
cht527 <cht527@126.com>
2025-07-18 16:49:18 +0800

    feat: update potentialList

--- src/pages/potential_list/computed.js
+++ src/pages/potential_list/computed.js
@@ -1,4 +1,124 @@
+const computeTotalCount = (dateRange, currentRangeData, name) =>
+  dateRange
+    .map((date) => {
+      const uniqueData = [
+        ...new Set(
+          currentRangeData
+            .filter((item) => item["月份"] === date)
+            .map((item) => item[name])
+        ),
+      ];
 
-const computedTrendData = (data, type)=>{
-   
-}
+      if (name === "品牌") {
+        return uniqueData.length;
+      }
+
+      if (name === "门店数") {
+        return uniqueData.reduce((sum, storeCount) => sum + storeCount, 0);
+      }
+    })
+    .reduce((sum, monthCount) => sum + monthCount, 0);
+
+const computeIngredientCount = (
+  dateRange,
+  currentRangeData,
+  ingredient,
+  name
+) =>
+  dateRange
+    .map((date) => {
+      const currentMonthData = currentRangeData.filter(
+        (item) => item["月份"] === date && item["加工后成分"] === ingredient
+      );
+
+      const uniqueList = [
+        ...new Set(currentMonthData.map((item) => item[name])),
+      ];
+      if (name === "品牌") {
+        return uniqueList.length;
+      }
+
+      if (name === "门店数") {
+        return uniqueList.reduce((sum, storeCount) => sum + storeCount, 0);
+      }
+    })
+    .reduce((sum, monthCount) => sum + monthCount, 0);
+
+const computedPotentialData = (currentRangeData, previousData) => {
+  const previousIngredients = [
+    ...new Set(previousData.map((item) => item["加工后成分"])),
+  ];
+  const currentRangeIngredients = [
+    ...new Set(currentRangeData.map((item) => item["加工后成分"])),
+  ];
+
+  const currentRangeNewIngredients = currentRangeIngredients.filter(
+    (ingredient) => !previousIngredients.includes(ingredient)
+  );
+
+  const dateRange = [...new Set(currentRangeData.map((item) => item["月份"]))];
+
+  // 计算 score = (统计周期内的) 上新该成分的品牌数 / 监测总品牌数 * 0.5 + 上新该成分的品牌门店数 / 监测品牌总门店数 * 0.5
+
+  // 监测总品牌数
+  const totalCurrentRangeBrands = computeTotalCount(
+    dateRange,
+    currentRangeData,
+    "品牌"
+  );
+
+  // 监测品牌总门店数
+  const totalCurrentRangeStores = computeTotalCount(
+    dateRange,
+    currentRangeData,
+    "门店数"
+  );
+
+  // 上新该成分的品牌数
+  const ingredientCountList = currentRangeNewIngredients.map((ingredient) => {
+    const brandCount = computeIngredientCount(
+      dateRange,
+      currentRangeData,
+      ingredient,
+      "品牌"
+    );
+
+    const storeCount = computeIngredientCount(
+      dateRange,
+      currentRangeData,
+      ingredient,
+      "门店数"
+    );
+
+    const score =
+      ((brandCount / totalCurrentRangeBrands) * 0.5 +
+        (storeCount / totalCurrentRangeStores) * 0.5) *
+      100;
+    return {
+      ingredient,
+      score,
+      tableRow: [
+        { name: "ingredient", value: ingredient },
+        { name: "brandCount", value: brandCount },
+        { name: "storeCount", value: storeCount },
+        { name: "score", value: score },
+      ],
+    };
+  });
+
+  const sortedIngredientList = ingredientCountList.sort(
+    (prev, next) => next["score"] - prev["score"]
+  );
+
+  const no20Ingredient = sortedIngredientList.at(19);
+
+  if (no20Ingredient !== undefined) {
+    const lastNo20ScoreIndex = sortedIngredientList.findLastIndex(
+      (item) => item.score === no20Ingredient.score
+    );
+    
+    return sortedIngredientList.slice(0, lastNo20ScoreIndex);
+  } else {
+    return sortedIngredientList.slice(0, 20);
+  }
+};
--- src/pages/potential_list/model.js
+++ src/pages/potential_list/model.js
@@ -1,34 +1,40 @@
-
 const { setOptionConfig } = window.parent.echartsVariables;
 
-class PotentialIngredientList  {
+class PotentialIngredientList {
   data = [];
-  ingredientDropInstance = null;
+  dateRange = [];
+  potentialListInstance = null;
   computedData = {
+    previousData: [],
     startDateIndex: 0,
     endDateIndex: 0,
     currentRangeData: [],
-    productSelectOptions: [],
   };
 
   element = {
     $datePicker: document.querySelector("#potential-ingredient-date-picker"),
+    $potentialListSection: document.querySelector("#potentialListSection"),
     $potentialList: document.querySelector("#potentialList"),
+    $potentialListTable: document.querySelector("#potentialListTable"),
+    $potentialListTbody: document.querySelector("#potentialListTbody"),
     $emptySection: document.querySelector("#emptySection"),
     $pageLoading: document.querySelector("#pageLoading"),
+    $potentialListLoading: document.querySelector("#potentialListLoading"),
   };
   constructor(initData) {
     this.init(initData);
-    this.setup();
     this.bind();
+    this.setup();
   }
 
   init = (initData) => {
     this.data = initData;
 
-    const dateRange = [...new Set(this.data.map((item) => item["月份"]))];
+    const dateRange = (this.dateRange = [
+      ...new Set(this.data.map((item) => item["月份"])),
+    ]);
     const lastDate = dateRange[dateRange.length - 1];
-    const startDate = dateRange[dateRange.length - 13];
+    const startDate = dateRange[dateRange.length - 6];
     this.element.$datePicker.value = `${startDate} 至 ${lastDate}`;
     this.element.$datePicker.min = dateRange[0];
     this.element.$datePicker.max = lastDate;
@@ -36,23 +42,19 @@ class PotentialIngredientList  {
     const endDateIndex = this.data.findLastIndex((d) => d["月份"] === lastDate);
 
     this.set({
-      startDateIndex,
-      endDateIndex,
+      currentRangeData: this.data.slice(startDateIndex, endDateIndex),
+      previousData: this.data.slice(0, startDateIndex),
     });
 
-    this.updateData();
-
-    this.newProductTrendInstance = window.parent.echarts.init(
-      this.element.$productTrend
+    this.potentialListInstance = window.parent.echarts.init(
+      this.element.$potentialList
     );
 
     this.element.$pageLoading.classList.add("hide");
   };
 
-
   setup() {
-    this.renderHeaderSelect();
-    this.renderNewProductBrandTrend(PRODUCT_NAME);
+    this.renderPotentialList();
   }
 
   get = (...keys) =>
@@ -73,59 +75,10 @@ class PotentialIngredientList  {
 
   bind = () => {
     const instance = this;
+
     this.element.$datePicker.addEventListener("change", function () {
       instance.dateChangeHandler(this.value);
     });
-
-    this.element.$bigProductTypeSelect.addEventListener(
-      "change",
-      this.bigProductTypeSelectChangeHandler
-    );
-
-    document
-      .getElementById("ingredientSelectContainer")
-      .addEventListener("click", this.ingredientButtonSelectHandler);
-
-    document
-      .getElementById("ingredientSelectContainer")
-      .addEventListener("keyup", this.ingredientSearchHandler);
-
-    document
-      .getElementById("ingredientSelectContainer")
-      .addEventListener("input", this.ingredientSearchInputHandler);
-
-    document
-      .querySelector(`#ingredientSelectContainer`)
-      .addEventListener("click", this.ingredientSelectHandler);
-
-    document.addEventListener("click", this.hidePanel);
-
-    document
-      .getElementById("productTypeSelect")
-      .addEventListener("click", this.productButtonSelectHandler);
-
-    document
-      .getElementById("productTypeSelect")
-      .addEventListener("click", (e) =>
-        this.productSelectedHandler(e, PRODUCT_SELECT_BUTTON_ID)
-      );
-
-    super.observe(this.element.$contentWrapper, () => {
-      this.newProductTrendInstance.resize();
-      this.brandTrendInstance.resize();
-    });
-  };
-
-  renderFilter() {
-    this.renderHeaderSelect();
-    this.renderIngredientSelectPanelComponent();
-    this.renderProductSelectComponent();
-  }
-
-  reRender = () => {
-    // 重新渲染
-    this.renderNewProductBrandTrend(PRODUCT_NAME);
-    this.renderNewProductBrandTrend(BRAND_NAME);
   };
 
   dateChangeHandler(dateRange) {
@@ -135,108 +88,71 @@ class PotentialIngredientList  {
     const startDateIndex = this.data.findIndex((d) => d["月份"] === startDate);
     const endDateIndex = this.data.findLastIndex((d) => d["月份"] === endDate);
     this.set({
-      startDateIndex,
-      endDateIndex,
+      currentRangeData: this.data.slice(startDateIndex, endDateIndex),
+      previousData: this.data.slice(0, startDateIndex),
     });
-    this.updateData();
 
-    this.renderFilter();
-
-    this.reRender();
+    this.renderPotentialList();
   }
 
-  // 产品大类选择
-  bigProductTypeSelectChangeHandler = (e) => {
-    this.set({
-      bigProductTypeValue: e.target.value,
-    });
-
-    this.updateData();
-    this.renderFilter();
-    this.reRender();
-  };
-
-  updateData() {
-    const { startDateIndex, endDateIndex } = this.get(
-      "startDateIndex",
-      "endDateIndex",
-    );
-
-    const filteredData = computedCurrentDataAndRange({
-      data: this.data,
-      startDateIndex,
-      endDateIndex,
-      bigProductTypeValue,
-    });
+  renderPotentialList = () => {
+    this.element.$potentialListLoading.classList.remove("hide");
 
 
-  }
+    setTimeout(() => {
+      this.element.$potentialListTbody.innerHTML = null;
 
-  updateCurrentRangeData() {
-    const {
-      startDateIndex,
-      endDateIndex,
-      selectedIngredient,
-      productTypeValue,
-      bigProductTypeValue,
-    } = this.get(
-      "startDateIndex",
-      "endDateIndex",
-      "selectedIngredient",
-      "productTypeValue",
-      "bigProductTypeValue"
-    );
+      const sectionEle = this.element.$potentialListSection;
 
-    const currentData = computedCurrentDataAndRange({
-      data: this.data,
-      startDateIndex,
-      endDateIndex,
-      bigProductTypeValue,
-      productTypeValue,
-    });
+      const tableEle = this.element.$potentialListTable;
 
-    this.set({
-      currentRangeData: currentData.filter(
-        (item) => item["加工后成分"] === selectedIngredient
-      ),
-    });
-  }
-
-  // 绘制数量趋势
-  renderNewProductBrandTrend = (name) => {
-    const isBrand = name === BRAND_NAME;
-    const emptySectionId = isBrand ? "brandSection" : "productSection";
-    const sectionEle = isBrand
-      ? this.element.$brandSection
-      : this.element.$productSection;
-    const chartEle = isBrand
-      ? this.element.$brandTrend
-      : this.element.$productTrend;
-    const chartInstance = isBrand
-      ? this.brandTrendInstance
-      : this.newProductTrendInstance;
-    const { currentRangeData } = this.get("currentRangeData");
-    
-    const emptySection = document.querySelector(
-      `#${emptySectionId} .empty-content`
-    );
-    if (emptySection) {
-      sectionEle.removeChild(emptySection);
-    }
-
-    if (currentRangeData.length === 0) {
-      chartEle.classList.add("hide");
-      sectionEle.appendChild(
-        this.element.$emptySection.content.cloneNode(true)
+      const { currentRangeData, previousData } = this.get(
+        "currentRangeData",
+        "previousData"
       );
-    } else {
-      chartEle.classList.remove("hide");
 
-      const trendData = computedTrendData(currentRangeData, name);
-      chartInstance.setOption(
-        getTrendOptions({ ...trendData }),
-        setOptionConfig
+      const emptySection = document.querySelector(
+        "#potentialListSection .empty-content"
       );
-    }
+      if (emptySection) {
+        sectionEle.removeChild(emptySection);
+      }
+
+      if (currentRangeData.length === 0) {
+        sectionEle.appendChild(
+          this.element.$emptySection.content.cloneNode(true)
+        );
+      } else {
+
+        const potentialData = computedPotentialData(
+          currentRangeData,
+          previousData
+        );
+
+        if (potentialData.length === 0) {
+          this.element.$potentialListTbody.appendChild(
+            this.element.$emptySection.content.cloneNode(true)
+          );
+        } else {
+          const tbodyFragment = document.createDocumentFragment();
+          potentialData.forEach((item) => {
+            const tr = document.createElement("tr");
+            item.tableRow.forEach(({name, value}) => {
+              const td = document.createElement("td");
+              const digits = name === 'score' ? 2 : 0;
+              td.innerHTML = name === 'ingredient' ? value : new Intl.NumberFormat('en-US',{
+                minimumFractionDigits:digits,
+                maximumFractionDigits:digits
+              }).format(value)
+              tr.appendChild(td);
+            });
+            tbodyFragment.appendChild(tr);
+          });
+          this.element.$potentialListTbody.appendChild(tbodyFragment);
+        }
+
+        this.element.$potentialListLoading.classList.add("hide");
+      }
+    }, 1500);
   };
 }
