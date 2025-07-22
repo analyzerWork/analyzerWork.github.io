cb7272653e167a100fb0b3a7e6843e404bdd983a
cht527 <cht527@126.com>
2025-07-22 18:18:09 +0800

    feat: add select

--- src/pages/potential_list/model.js
+++ src/pages/potential_list/model.js
@@ -4,15 +4,20 @@ class PotentialIngredientList {
   data = [];
   dateRange = [];
   potentialListInstance = null;
+  bigProductTypeOptions = ["茶饮", "咖啡"];
   computedData = {
     previousData: [],
     startDateIndex: 0,
     endDateIndex: 0,
     currentRangeData: [],
+    bigProductTypeValue: "茶饮",
+    selectedIngredientClass: '',
   };
 
   element = {
     $datePicker: document.querySelector("#potential-ingredient-date-picker"),
+    $bigProductTypeSelect: document.querySelector("#bigProductTypeSelect"),
+    $potentialIngredientSelect: document.querySelector("#potentialIngredientSelect"),
     $potentialListSection: document.querySelector("#potentialListSection"),
     $potentialList: document.querySelector("#potentialList"),
     $potentialListTable: document.querySelector("#potentialListTable"),
@@ -43,6 +48,8 @@ class PotentialIngredientList {
 
     this.set({
       currentRangeData: this.data.slice(startDateIndex, endDateIndex),
+      startDateIndex,
+      endDateIndex,
       previousData: this.data.slice(0, startDateIndex),
     });
 
@@ -54,6 +61,7 @@ class PotentialIngredientList {
   };
 
   setup() {
+    this.renderHeaderSelect();
     this.renderPotentialList();
   }
 
@@ -79,21 +87,121 @@ class PotentialIngredientList {
     this.element.$datePicker.addEventListener("change", function () {
       instance.dateChangeHandler(this.value);
     });
+
+    this.element.$bigProductTypeSelect.addEventListener(
+      "change",
+      this.bigProductTypeSelectChangeHandler
+    );
+
+    this.element.$potentialIngredientSelect.addEventListener(
+      "change",
+      this.potentialIngredientSelectChangeHandler
+    );
   };
 
   dateChangeHandler(dateRange) {
+
     const [startDate, endDate] = dateRange
       .split("至")
       .map((value) => value.trim());
     const startDateIndex = this.data.findIndex((d) => d["月份"] === startDate);
     const endDateIndex = this.data.findLastIndex((d) => d["月份"] === endDate);
     this.set({
-      currentRangeData: this.data.slice(startDateIndex, endDateIndex),
-      previousData: this.data.slice(0, startDateIndex),
+      startDateIndex,
+      endDateIndex,
     });
+
+    // 所选时间段内可能没有当前的成分分类，所以要重新计算
+    this.renderHeaderSelect(); 
 
     this.renderPotentialList();
   }
+
+  renderHeaderSelect() {
+    const { bigProductTypeValue, startDateIndex,endDateIndex } = this.get(
+      "bigProductTypeValue",
+      "startDateIndex",
+      "endDateIndex"
+    );
+    if (this.bigProductTypeOptions.length > 0) {
+      const menuFragment = computedMenuOptionsFragment(
+        this.bigProductTypeOptions
+      );
+      this.element.$bigProductTypeSelect.innerHTML = null;
+      this.element.$bigProductTypeSelect.appendChild(menuFragment);
+      this.element.$bigProductTypeSelect.value = bigProductTypeValue;
+    }
+
+    const currentBigProductTypeData = computedCurrentDataAndRange({
+      data: this.data,
+      startDateIndex,
+      endDateIndex,
+      bigProductTypeValue,
+    });
+    const previousDataBigProductTypeData = computedCurrentDataAndRange({
+      data: this.data,
+      startDateIndex:0,
+      endDateIndex:startDateIndex,
+      bigProductTypeValue,
+    });
+
+    const ingredientClassOptions = [...new Set(currentBigProductTypeData.map((item)=>item[`成分分类-${bigProductTypeValue}`]))];
+    const ingredientClassMenuFragment = computedMenuOptionsFragment(
+      ingredientClassOptions
+    );
+    this.element.$potentialIngredientSelect.innerHTML = null;
+    this.element.$potentialIngredientSelect.appendChild(ingredientClassMenuFragment);
+    const selectedIngredientClass= ingredientClassOptions[0] || "";
+    this.element.$potentialIngredientSelect.value = selectedIngredientClass;
+
+    this.set({
+      selectedIngredientClass,
+      currentRangeData: currentBigProductTypeData.filter(item => item[`成分分类-${bigProductTypeValue}`] === selectedIngredientClass),
+      previousData: previousDataBigProductTypeData.filter(item => item[`成分分类-${bigProductTypeValue}`] === selectedIngredientClass)
+    })
+
+  }
+
+  // 产品大类选择
+  bigProductTypeSelectChangeHandler = (e) => {
+    this.set({
+      bigProductTypeValue: e.target.value,
+    });
+
+    this.renderHeaderSelect();
+  
+    this.renderPotentialList();
+  };
+
+  potentialIngredientSelectChangeHandler = (e) => {
+    const selectedIngredientClass= e.target.value || '';
+
+    const { bigProductTypeValue, startDateIndex,endDateIndex } = this.get(
+      "bigProductTypeValue",
+      "startDateIndex",
+      "endDateIndex"
+    );
+    const currentBigProductTypeData = computedCurrentDataAndRange({
+      data: this.data,
+      startDateIndex,
+      endDateIndex,
+      bigProductTypeValue,
+    });
+    const previousDataBigProductTypeData = computedCurrentDataAndRange({
+      data: this.data,
+      startDateIndex:0,
+      endDateIndex:startDateIndex,
+      bigProductTypeValue,
+    });
+
+    this.set({
+      selectedIngredientClass,
+      currentRangeData: currentBigProductTypeData.filter(item => item[`成分分类-${bigProductTypeValue}`] === selectedIngredientClass),
+      previousData: previousDataBigProductTypeData.filter(item => item[`成分分类-${bigProductTypeValue}`] === selectedIngredientClass)
+    });
+
+    this.renderPotentialList();
+  };
 
   renderPotentialList = () => {
     this.element.$potentialListLoading.classList.remove("hide");
@@ -132,8 +240,13 @@ class PotentialIngredientList {
 
       setTimeout(() => {
         if (potentialData.length === 0) {
+          const tr = document.createElement('tr');
+          const td =  document.createElement('td');
+          td.appendChild(this.element.$emptySection.content.cloneNode(true))
+          td.colSpan = 4
+          tr.appendChild(td)
           this.element.$potentialListTbody.appendChild(
-            this.element.$emptySection.content.cloneNode(true)
+            tr
           );
         } else {
           const tbodyFragment = document.createDocumentFragment();
